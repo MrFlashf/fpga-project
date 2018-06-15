3e5d607938f66fc74c9d14cb3300cf8301561427
Krzysztof Jozefowicz <krzysztof.jozefowicz@appunite.com>
2018-06-15 21:39:45 +0200

    Init

---
+++ src/calcu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <main.h>
+
+void print_calc(void) {
+
+  printf("Hello world!\n");
+
+  return;
+}
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <main.h>
+
+// 1. getoptem wziąć jakie działanie matematyczne
+// 2. getoptem wziąć nazwe pliku
+// 3. sparsować plik do arraya
+// 4. wykonać działanie
+// 5. wyswietlic wynik
+int main(int argc, char *argv[]) {
+  print_calc();
+}
