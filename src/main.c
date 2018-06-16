7cb00aa864afe7820349f48e4fb87279e421bc83
Krzysztof Józefowicz <krz.jozefowicz@gmail.com>
2018-06-16 12:16:16 +0200

    Remove object folder from git

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <main.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 // 1. getoptem wziąć jakie działanie matematyczne
 // 2. getoptem wziąć nazwe pliku
@@ -8,6 +10,25 @@
 // 5. wyswietlic wynik
 int main(int argc, char *argv[]) {
   int result;
+  char opt;
+  char* type;
+  char* fileName;
+
+
+  while((opt = getopt(argc, argv, "t:f:")) != -1) {
+    switch(opt) {
+      case 't':
+        type = optarg;
+        printf("Type = %s\n", type);
+        break;
+      case 'f':
+        fileName = optarg;
+        printf("Filename: %s\n", fileName);
+        break;
+      default: 
+        exit(1);
+    }
+  }
 
   // zamiast hello.txt daj tutaj nazwe pliku z getopt
   struct Tuple t = getNumbersFromFile("hello.txt");
@@ -15,7 +36,10 @@ int main(int argc, char *argv[]) {
   // zamiast 1 daj tutaj z getopta cyfrę odpowiadającą za rodzaj operacji, np. 1 dodawanie, 2 mnożenie itd.
   result = count(t, 1);
 
-
-  printf("Wynik: %d\n", result);
+  if (result == NULL) {
+    printf("BŁĄD! Nie można dzielić przez zero!");
+  } else {
+    printf("Wynik: %d\n", result);
+  }
   return(0);
 }
