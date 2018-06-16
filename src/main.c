8d0d7257f72e4f927368d97d17e5f78d01b3065e
Krzysztof Józefowicz <krz.jozefowicz@gmail.com>
2018-06-16 11:22:08 +0200

    Add parsing file

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <main.h>
+#include <stdio.h>
 
 // 1. getoptem wziąć jakie działanie matematyczne
 // 2. getoptem wziąć nazwe pliku
@@ -8,5 +8,18 @@
 // 5. wyswietlic wynik
 int main(int argc, char *argv[]) {
   print_calc();
-  getNumbersFromFile("hello.txt");
+  struct Tuple t = getNumbersFromFile("hello.txt");
+
+  int a = t.howManyLines;
+
+  int wynik = 1;
+
+  for (int i = 0; i < t.howManyLines; i++) {
+    printf("Liczba %d: %d\n", i, t.numbers[i]);
+    wynik = wynik*t.numbers[i];
+  }
+
+
+
+  printf("luine number %d\n", wynik);
 }
--- src/parser.c
+++ src/parser.c
@@ -1,16 +1,31 @@
-int getNumbersFromFile(fileName) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+
+struct Tuple getNumbersFromFile(char *fileName) {
   FILE *fp;
+  char *line = NULL;
   char buff[255];
+  size_t len = 0;
+  int array[999];
+  int lineNumber = 0;
+
 
   fp = fopen(fileName, "r");
-  fscanf(fp, "%s", buff);
-  printf("1 : %s\n", buff );
 
-  fgets(buff, 255, (FILE*)fp);
-  printf("2: %s\n", buff );
-  
-  fgets(buff, 255, (FILE*)fp);
-  printf("3: %s\n", buff );
+  while((getline(&line, &len, fp)) != -1) {
+    int currentNumber = atoi(line);
+    array[lineNumber] = currentNumber;
+    lineNumber += 1;
+  }
+
+  for (int i=0; i < lineNumber; i++) {
+    printf("Line: %d\n", array[i]);
+  }
+
   fclose(fp);
-  return(0);
+  struct Tuple ret = { array, lineNumber };
+  return ret;
+  
+  // return(0);
 }
